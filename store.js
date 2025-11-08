740c00bd359d94d4b61eb6173ce7e854fddedab8
Bhupesh Dewangan <bhupeshdewangan2004@gmail.com>
2025-11-08 19:14:03 +0530

    Delete store.js

--- store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "../redux/cartSlice.js";
-
-const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
-});
-export default store;
